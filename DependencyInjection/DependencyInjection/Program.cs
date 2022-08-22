316c9d57b42083d16f9b2b6c2092013a17a42697
Kit <kit.j.lau@outlook.com>
2022-08-22 17:47:30 +0800

    Feat: Add new Project DependencyInjection

---
+++ DependencyInjection/DependencyInjection/Program.cs
@@ -0,0 +1,21 @@
+﻿using DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+var services = new ServiceCollection();
+
+// services.AddSingleton<IService, Service>();
+
+var serviceDescriptor = new ServiceDescriptor(
+    typeof(IService), 
+    typeof(Service), 
+    ServiceLifetime.Singleton);
+
+services.Add(serviceDescriptor);
+
+var serviceProvider = services.BuildServiceProvider();
+
+var myService = serviceProvider.GetRequiredService<IService>();
+
+myService.DoIt();
+
---
+++ DependencyInjection/DependencyInjection/Service.cs
@@ -0,0 +1,15 @@
+﻿namespace DependencyInjection;
+
+public interface IService
+{
+    void DoIt();
+}
+
+public class Service : IService
+{
+    public void DoIt()
+    {
+        Console.WriteLine("Done!");
+    }
+}
+
